e7815aac7a378095f16f05e6c11f56ba738d0a3b
Abhishek kumar <abhi.arya97161@gmail.com>
2024-02-13 21:55:15 +0530

    regiser form frontend created

--- register_user.php
+++ register_user.php
@@ -1,13 +1,41 @@
 <?php
+include("./includes/header.php");
 
-include("./includes/header.php")
-
+$user_name = $_REQUEST['user_name'];
+$user_pass = $_REQUEST['user_pass'];
 ?>
 
 
 
 
+<div class="container">
+    <div class="card my-card">
+    <div class="card-header bg-primary text-white text-center">
+        Register User
+    </div>
+    <div class="card-body">
+       <div class="row">
+            <div class="col-12">
+               <form method="POST">
+                <div class="mb-3">
+                        <label for="" class="form-label">Username:</label>
+                        <input type="text" class="form-control" name="user_name" value="">
+                    </div>
 
+                    <div class="mb-3">
+                        <label for="" class="form-label">Password:</label>
+                        <input type="password" class="form-control" name="user_pass" value="" autocomplete="off">
+                    </div>
+
+                    <div class="mb-3">
+                        <button type="submit" name="register" class="btn btn-primary w-100">Submit</button>
+                    </div>
+               </form>
+            </div>
+       </div>
+    </div>
+    </div>
+</div>
 
 
 
@@ -21,7 +49,5 @@ include("./includes/header.php")
 
 
 <?php
-
 include("./includes/footer.php")
-
 ?>
